50b37bbd3a1b31a0f7e6bce5774eba8be7dca1f5
Mahwash Mahr <mahwash.mahr@gmail.com>
2018-02-13 15:32:29 -0500

    updated css

--- script.js
+++ script.js
@@ -11,12 +11,27 @@ function checkAnswers () {
     var correctAnswer = document.querySelector('#correct')
     var i
         for(i = 0; i < correctAnswer.length; i++) {
-            correctAnswer[i].style.color = 'red'
+            correctAnswer[i].style.color = 'green'
         }
-
-
-
-       // identify element that is the correct answer through DOM manipulation
+       
+        // identify element that is the correct answer through DOM manipulation
 }
 
-  // check user input to see if it is the same as the correct answer
+  
+//capture user input in an arry?
+// check user input to see if it is the same as the correct answer
+  // if user click the right answer turn green
+  // else turn red
+
+
+/*
+
+- add change event listener to all forms with 1 event handler
+- on change:
+    - check target for selected input (google how to do that)
+    - if selected input has name of "correct":
+        - add .correct class to form (event target)
+    - else:
+        - add .incorrect class to form (event target)
+
+*/
