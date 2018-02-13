e9bcee1bc07e18686ad3b502848c55d9a620a0c9
Mahwash Mahr <mahwash.mahr@gmail.com>
2018-02-13 09:16:45 -0500

    still working on logic

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-var score = 0
-var points = score + 5
+console.log('Hello World')
+
+
+//var score = 0
+//var points = score + 5
 
 var submit = document.querySelector('#submit')
 submit.addEventListener('click', checkAnswers)
 
 function checkAnswers () {
+    var correctAnswer = document.querySelector('#correct')
+    var i
+        for(i = 0; i < correctAnswer.length; i++) {
+            correctAnswer[i].style.color = 'red'
+        }
 
+
+
+       // identify element that is the correct answer through DOM manipulation
 }
+
+  // check user input to see if it is the same as the correct answer
