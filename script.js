dfc998256902b0de3f431f6cca934666f3c8c310
Mahwash Mahr <mahwash.mahr@gmail.com>
2018-02-14 13:38:01 -0500

    made changes to script file

--- script.js
+++ script.js
@@ -1,37 +1,118 @@
 console.log('Hello World')
 
+const aButton = document.querySelector('#a')
+const bButton = document.querySelector('#b')
+const cButton = document.querySelector('#c')
 
-//var score = 0
-//var points = score + 5
+const dButton = document.querySelector('#d')
+const eButton = document.querySelector('#e')
+const fButton = document.querySelector('#f')
 
-var submit = document.querySelector('#submit')
-submit.addEventListener('click', checkAnswers)
+const gButton = document.querySelector('#g')
+const hButton = document.querySelector('#h')
+const iButton = document.querySelector('#i')
 
-function checkAnswers () {
-    var correctAnswer = document.querySelector('#correct')
-    var i
-        for(i = 0; i < correctAnswer.length; i++) {
-            correctAnswer[i].style.color = 'green'
-        }
-       
-        // identify element that is the correct answer through DOM manipulation
-}
+const jButton = document.querySelector('#j')
+const kButton = document.querySelector('#k')
+const lButton = document.querySelector('#l')
 
-  
-//capture user input in an arry?
-// check user input to see if it is the same as the correct answer
-  // if user click the right answer turn green
-  // else turn red
+const mButton = document.querySelector('#m')
+const nButton = document.querySelector('#n')
+const oButton = document.querySelector('#o')
+
+const pButton = document.querySelector('#p')
+const qButton = document.querySelector('#q')
+const rButton = document.querySelector('#r')
+
+const sButton = document.querySelector('#s')
+const tButton = document.querySelector('#t')
+const uButton = document.querySelector('#u')
+
+//const correctAnswers = ['cButton', 'eButton','gButton', 'kButton','oButton','pButton', 'uButton']
+//cont wrongAnswers = ['aButton', 'bButton', 'fButton', 'dButton', 'dButton', 'hButton', 'iButton',
+//'jButton', 'lButton', 'mButton', 'nButton', 'qButton', 'rButton', 'sButton', 'tButton']
+
+aButton.addEventListener('click', question1)
+bButton.addEventListener('click', question1)
+cButton.addEventListener('click', question1)
 
 
-/*
+ function question1() {
+    if (this.innerHTML === '500,000') {
+        this.style.backgroundColor = 'green'
+    } else {
+        this.style.backgroundColor = 'red'
+    }
+ }
+ 
+dButton.addEventListener('click', question2)
+eButton.addEventListener('click', question2)
+fButton.addEventListener('click', question2)
 
-- add change event listener to all forms with 1 event handler
-- on change:
-    - check target for selected input (google how to do that)
-    - if selected input has name of "correct":
-        - add .correct class to form (event target)
-    - else:
-        - add .incorrect class to form (event target)
+ function question2() {
+    if (this.innerHTML === '30,000') {
+        this.style.backgroundColor = 'green'
+    } else {
+        this.style.backgroundColor = 'red'
+    }
+ }
 
-*/
+gButton.addEventListener('click', question3)
+hButton.addEventListener('click', question3)
+iButton.addEventListener('click', question3)
+
+ function question3() {
+    if (this.innerHTML === '5') {
+        this.style.backgroundColor = 'green'
+    } else {
+        this.style.backgroundColor = 'red'
+    }
+ }
+
+jButton.addEventListener('click', question4)
+kButton.addEventListener('click', question4)
+lButton.addEventListener('click', question4)
+
+function question4() {
+    if (this.innerHTML === '3') {
+        this.style.backgroundColor = 'green'
+    } else {
+        this.style.backgroundColor = 'red'
+    }
+ }
+
+mButton.addEventListener('click', question5)
+nButton.addEventListener('click', question5)
+oButton.addEventListener('click', question5)
+
+function question5() {
+    if (this.innerHTML === 'Both') {
+        this.style.backgroundColor = 'green'
+    } else {
+        this.style.backgroundColor = 'red'
+    }
+ }
+
+pButton.addEventListener('click', question6)
+qButton.addEventListener('click', question6)
+rButton.addEventListener('click', question6)
+
+function question6() {
+    if (this.innerHTML === '850 - 3,500 kg') {
+        this.style.backgroundColor = 'green'
+    } else {
+        this.style.backgroundColor = 'red'
+    }
+ }
+
+sButton.addEventListener('click', question7)
+tButton.addEventListener('click', question7)
+uButton.addEventListener('click', question7)
+
+function question7() {
+    if (this.innerHTML === 'Both') {
+        this.style.backgroundColor = 'green'
+    } else {
+        this.style.backgroundColor = 'red'
+    }
+ }
